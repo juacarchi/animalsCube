7487286bbe9202219e8aa245fa98192b8caff1f6
juacarchi <cardenasfanoce@gmail.com>
2021-05-24 14:11:34 +0200

    NewCommit

--- JuegoAnimales/Assets/Scripts/GameplayManager.cs
+++ JuegoAnimales/Assets/Scripts/GameplayManager.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,19 +6,19 @@ public class GameplayManager : MonoBehaviour
 {
     public GridLayoutGroup gridLayout;
     public Transform parentPanel;
-    public GameObject[] posLettersToComplete;
-    public GameObject[] posLettersGrid;
-    public GameObject panelVictory; 
-    
+    public RectTransform[] posLettersToComplete;
+    public RectTransform[] posLettersGrid;
+    public GameObject panelVictory;
+
     public SpriteRenderer animalImage;
     public Sprite[] spriteBackground;
-    
+
     public Image imageFondo;
     GameObject[] lettersToComplete;
     GameObject[] cubeLetters;
     Sprite animalSprite;
     Animal newAnimal;
-    List <GameObject> animalsList;
+    List<GameObject> animalsList;
 
     int n_animal;
     private void Start()
@@ -28,13 +27,13 @@ public class GameplayManager : MonoBehaviour
         animalsList = GameManager.instance.GetAnimalList();
         RandomAnimal();
 
-        
+
     }
     public void RandomAnimal()
     {
         int backgroundNumber = Random.Range(0, spriteBackground.Length);
         imageFondo.sprite = spriteBackground[backgroundNumber];
-        n_animal= Random.Range(0, animalsList.Count);
+        n_animal = Random.Range(0, animalsList.Count);
         GameManager.instance.SetNumberOfAnimal(n_animal);
 
         newAnimal = animalsList[n_animal].GetComponent<Animal>();
@@ -44,13 +43,15 @@ public class GameplayManager : MonoBehaviour
         cubeLetters = newAnimal.GetLettersAnimal();
         for (int i = 0; i < lettersToComplete.Length; i++)
         {
-            GameObject spaceLetter = Instantiate(lettersToComplete[i], posLettersToComplete[i].transform.position, Quaternion.identity) as GameObject;
+            GameObject spaceLetter = Instantiate(lettersToComplete[i], posLettersToComplete[i].anchoredPosition, Quaternion.identity);
             spaceLetter.transform.parent = posLettersToComplete[i].transform;
+            spaceLetter.transform.localScale = new Vector3(1, 1, 1);
         }
         for (int i = 0; i < cubeLetters.Length; i++)
         {
-            GameObject letter = Instantiate(cubeLetters[i], Vector3.zero, Quaternion.identity) as GameObject;
+            GameObject letter = Instantiate(cubeLetters[i], posLettersGrid[i].anchoredPosition, Quaternion.identity);
             letter.transform.SetParent(parentPanel);
+            letter.transform.localScale = new Vector3(1, 1, 1);
         }
 
         animalImage.sprite = animalSprite;
